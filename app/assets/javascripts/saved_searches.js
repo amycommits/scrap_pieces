8305fd21b9d6aeaee9fee6bf9457a02513df6cbb
Amy <detwiler.amy@gmail.com>
2016-06-08 02:55:51 +0000

    recommended changes

--- app/assets/javascripts/saved_searches.js
+++ app/assets/javascripts/saved_searches.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-    $("#new_saved_search").click(function() {
+  /*  $("#new_saved_search").click(function() {
         //$('saved_searchs/form').trigger('submit.rails')
         $(this).submit();
     }
     );
     //$('#new_saved_search').hide().after('');
-    
+   */ 
 });
